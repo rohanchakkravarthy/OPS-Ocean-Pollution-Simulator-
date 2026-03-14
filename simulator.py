fc0ec9133add08ce64d2b808d1bb69aafb66c6e4
rohanchakkravarthy <rohanchakkravarthy@gmail.com>
2026-03-14 12:43:17 -0400

    Add simulator

---
+++ simulator.py
@@ -0,0 +1,9 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+WIDTH, HEIGHT = 900, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Ocean Pollution Simulator")
